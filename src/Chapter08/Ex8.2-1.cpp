34635332d9935d1bf22d17920671f173d4d5de1b
Robin SHAMSNEJAD <shamsnejad@digigram.com>
2020-11-26 13:23:23 +0100

    Add Chapter 08 exercises

---
+++ src/Chapter08/Ex8.2-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class IntPair
+{
+public:
+	int m_int1;
+	int m_int2;
+	
+	void set(int int1, int int2)
+	{
+		m_int1 = int1;
+		m_int2 = int2;
+	}
+	void print(void)
+	{
+		std::cout <<
+			"Pair(" << m_int1 << ", " << m_int2 << ")"
+		<< std::endl;
+	}
+};
+
+int main(int argc, char **argv)
+{
+	IntPair p1;
+	p1.set(1, 1);
+	IntPair p2{2, 2};
+
+	p1.print();
+	p2.print();
+	
+	return EXIT_SUCCESS;
+}
